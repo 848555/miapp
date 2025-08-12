9c356ebde2b7959b997300e002218f578035be89
848555 <163752758+848555@users.noreply.github.com>
2025-08-12 17:53:29 -0500

    Update documentos_mototaxistas_usu.php
    se recepciona mensajes

--- app/pages/documentos_mototaxistas_usu.php
+++ app/pages/documentos_mototaxistas_usu.php
@@ -50,6 +50,19 @@ $documentos_moto = obtenerDatos($conexion, $id_usuario);
       <!-- Formulario pre-llenado con los datos actuales. Se envía a /app/include/editar_docu.php -->
       <form  method="POST" action="/app/include/editar_docu.php" enctype="multipart/form-data">
         <!-- Campos de texto para datos básicos -->
+
+          <?php
+session_start();
+if (isset($_SESSION['success_message'])) {
+    echo "<div class='alert alert-success'>" . $_SESSION['success_message'] . "</div>";
+    unset($_SESSION['success_message']);
+}
+if (isset($_SESSION['error_message'])) {
+    echo "<div class='alert alert-danger'>" . $_SESSION['error_message'] . "</div>";
+    unset($_SESSION['error_message']);
+}
+?>
+
         <label for="placa"><strong>Placa:</strong></label>
         <input type="text" id="placa" name="placa" value="<?php echo htmlspecialchars($documentos_moto['placa']); ?>" required>
         <br><br>
