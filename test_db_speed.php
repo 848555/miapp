4d4709206b550677032f690379fc2378c9417038
848555 <163752758+848555@users.noreply.github.com>
2025-08-14 22:32:54 -0500

    Create test_db_speed.php
    prueba de velocidad

---
+++ test_db_speed.php
@@ -0,0 +1,21 @@
+<?php
+$start_global = microtime(true);
+
+$start_conexion = microtime(true);
+include(__DIR__ . '/../../config/conexion.php'); 
+$end_conexion = microtime(true);
+
+$start_query = microtime(true);
+$result = $conexion->query("SELECT id_usuarios FROM usuarios LIMIT 1");
+$end_query = microtime(true);
+
+$end_global = microtime(true);
+
+echo "<pre>";
+echo "⏱ Tiempo conexión: " . round(($end_conexion - $start_conexion) * 1000, 2) . " ms\n";
+echo "⏱ Tiempo consulta: " . round(($end_query - $start_query) * 1000, 2) . " ms\n";
+echo "⏱ Tiempo total: " . round(($end_global - $start_global) * 1000, 2) . " ms\n";
+echo "</pre>";
+
+$conexion->close();
+?>
