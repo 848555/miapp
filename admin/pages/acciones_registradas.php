069c671130c1fbe3e509a0a9dc6ad24792a98b17
848555 <163752758+848555@users.noreply.github.com>
2025-08-15 16:32:24 -0500

    Update acciones_registradas.php
    se le agrega el nombre del proyecto

--- admin/pages/acciones_registradas.php
+++ admin/pages/acciones_registradas.php
@@ -37,7 +37,7 @@ $resultado_acciones = $conexion->query($sql_acciones);
   <div class="container-fluid">
     <a class="navbar-brand" href="/admin/pages/principal.php">
       <img src="/app/assets/imagenes/imagen.jpeg" alt="Logo" width="30" height="30">
-      ADMIN PANEL
+      MotoRide
     </a>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
