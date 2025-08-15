26235da8c8f491386eb2d60b0351622aa066c2fd
848555 <163752758+848555@users.noreply.github.com>
2025-08-14 22:46:58 -0500

    Update conexion.php
    se arregla la conexion para que No se vuelve a hacer el handshake TCP/MySQL en cada request.

--- config/conexion.php
+++ config/conexion.php
@@ -5,11 +5,14 @@ $contrasena = "xwQsSDDkSMi7jAS7a02f";
 $base_datos = "b4c1tgfsfzndtxfygnwk";
 $puerto = 3306;
 
-$conexion = new mysqli($host, $usuario, $contrasena, $base_datos, $puerto);
+$conexion = new mysqli('p:'.$host, $usuario, $contrasena, $base_datos, $puerto);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
-
+// Verifica que la conexión esté viva
+if (!$conexion->ping()) {
+    $conexion = new mysqli('p:'.$host, $usuario, $contrasena, $base_datos, $puerto);
+}
 $conexion->set_charset("utf8");
 ?>
