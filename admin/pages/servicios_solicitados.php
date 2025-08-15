2605609cdcfbb09da1ef37ec3aea22762f322853
848555 <163752758+848555@users.noreply.github.com>
2025-08-14 23:03:56 -0500

    Update servicios_solicitados.php
    se agrega validar si el usuario esta autenticado

--- admin/pages/servicios_solicitados.php
+++ admin/pages/servicios_solicitados.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+<?php
+session_start();
+// Verificar si 'usuario' está definido en la sesión
+if (!isset($_SESSION['usuario']) || $_SESSION['usuario'] === '') {
+    header("Location: ../../../../index.php");
+    exit;
+}
+include(__DIR__ . '../../../config/conexion.php');
 include(__DIR__ . '../../../config/conexion.php');
 
 // Respuesta AJAX para actualizar solo la tabla sin recargar
