6125f10c01b8c0ec05c9f5d47e53cac5e6810b82
848555 <163752758+848555@users.noreply.github.com>
2025-08-19 17:53:28 -0500

    Update script.js
    dxA

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 
-
 // Función para alternar visibilidad de la contraseña
-function togglePasswordVisibility(icon) {
-    var input = icon.previousElementSibling; // el input justo antes del icono
-
+function togglePasswordVisibility(id) {
+    var input = document.getElementById(id);
+    var icon = input.nextElementSibling;
     if (input.type === "password") {
         input.type = "text";
         icon.classList.remove("fa-eye");
@@ -14,6 +13,7 @@ function togglePasswordVisibility(icon) {
         icon.classList.add("fa-eye");
     }
 }
+
 // Función para validar las contraseñas
 function validarContraseñas() {
     var nuevaContraseña = document.getElementById("contraseña").value;
