d8003f3bbd9f52b6914ee74126107017d0d442ba
848555 <163752758+848555@users.noreply.github.com>
2025-08-31 10:48:32 -0500

    Update principal.php
    se corige

--- admin/pages/principal.php
+++ admin/pages/principal.php
@@ -55,6 +55,7 @@ $sql_acciones = "SELECT id_usuarios FROM usuarios";
     <title>Crud</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/35f3448c23.js" crossorigin="anonymous"></script>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
